431c059ce095e681366ac49a46e62c75b3e122f2
Dima <78323304+DimaFursov86@users.noreply.github.com>
2022-01-25 02:18:37 +0200

    adding email varification

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {BadRequest, Conflict, Unauthorized} = require("http-errors");
+const {BadRequest, NotFound, Conflict, Unauthorized} = require("http-errors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const {authenticate, upload} = require("../../middlewares");
@@ -7,15 +7,19 @@ const gravatar = require("gravatar");
 const fs = require("fs/promises");
 const path = require("path");
 const Jimp = require("Jimp");
+const { nanoid } = require("nanoid");
+
 
 const {User} = require("../../models");
-const {joiSignupSchema, joiLoginSchema} = require("../../models/user");
+const { joiSignupSchema, joiLoginSchema } = require("../../models/user");
+
+const {sendEmail} = require("../../helpers");
 
 const router = express.Router();
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
-const {SECRET_KEY} = process.env;
+const {SECRET_KEY, SITE_NAME} = process.env;
 
 router.post("/signup", async(req, res, next) => {
     try {
@@ -31,8 +35,17 @@ router.post("/signup", async(req, res, next) => {
       
         const salt = await bcrypt.genSalt(10);
         const hashPassword = await bcrypt.hash(password, salt);
+        const verificationToken = nanoid();
         const avatarURL = gravatar.url(email);
-        const newUser = await User.create({subscription, email, password: hashPassword, avatarURL});
+        const newUser = await User.create({ subscription, email, verificationToken, password: hashPassword, avatarURL });
+        
+         const data = {
+                to: email,
+                subject: "Подтверждение email",
+                html: `<a target="_blank" href="${SITE_NAME}/users/verify/${verificationToken}">Подтвердить email</a>`
+        }
+        await sendEmail(data);
+
         res.status(201).json({
             user: {
                 email: newUser.email,
@@ -75,6 +88,9 @@ router.post("/login", async (req, res, next) => {
         if (!user) {
             throw new Unauthorized("Email or password is wrong");
         }
+        if(!user.verify){
+            throw new Unauthorized("Email not verified");
+        }
         const passwordCompare = await bcrypt.compare(password, user.password);
         if (!passwordCompare) {
             throw new Unauthorized("Email or password is wrong");
@@ -104,14 +120,59 @@ router.get("/logout", authenticate, async(req, res)=> {
     res.status(204).send();
 });
 
-router.get("/current", authenticate, async(req, res)=> {
-    const {subscription, email} = req.user;
+router.get("/current", authenticate, async (req, res) => {
+    const { subscription, email } = req.user;
     res.json({
         user: {
             subscription,
             email
         }
     })
+});
+
+router.post("/verify", async(req, res, next)=> {
+    try {
+        const {email} = req.body;
+        if(!email){
+            throw new BadRequest("missing required field email");
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            throw new NotFound('User not found');
+        }
+        if(user.verify){
+            throw new BadRequest("Verification has already been passed")
+        }
+
+        const {verificationToken} = user;
+        const data = {
+            to: email,
+            subject: "Подтверждение email",
+            html: `<a target="_blank" href="${SITE_NAME}/users/verify/${verificationToken}">Подтвердить email</a>`
+        }
+
+        await sendEmail(data);
+
+        res.json({message: "Verification email sent"});
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.get("/verify/:verificationToken", async(req, res, next)=> {
+    try {
+        const {verificationToken} = req.params;
+        const user = await User.findOne({verificationToken});
+        if(!user){
+            throw new NotFound('User not found');
+        }
+        await User.findByIdAndUpdate(user._id, {verificationToken: null, verify: true});
+        res.json({
+            message: 'Verification successful'
+        })
+    } catch (error) {
+        next(error);
+    }
 })
 
 module.exports = router;
